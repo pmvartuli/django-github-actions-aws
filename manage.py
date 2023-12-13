cb08b22810145c5575dc4ca128fb2944bae429ed
Pat <github@patrickvartuli.com>
2023-12-13 17:22:12 +1100

    update test and gitignore

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    print("foobar")
+    print("foobar 99")
 
 
 if __name__ == '__main__':
